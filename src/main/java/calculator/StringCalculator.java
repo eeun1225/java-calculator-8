a3d77e6725f9f4199af93a5be5a2e67b8d3b3dc5
eeun1225 <eeun9108@gmail.com>
2025-10-20 10:07:25 +0900

    feat: null과 빈 문자열 입력 시 0을 반환하는 기능 구현
    - StringCalculator.calculate() 메서드에서 null, 빈 문자열 처리

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        return 0; // 임시 반환 값
+    }
+}
