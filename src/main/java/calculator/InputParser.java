9adb190f59d7713879cd691c5a6bc884ecdf9aef
eeun1225 <eeun9108@gmail.com>
2025-10-20 10:35:20 +0900

    feat: 커스텀 구분자 분리 기능 구현
    - InputParser.parse()에 커스텀 구분자 처리 로직 추가
    - InputParser.hasCustomDelimiter()로 커스텀 구분자 여부 확인
    - InputParser.extractCustomDelimiter()로 커스텀 구분자 추출
    - InputParser.removeDelimiterPrefix()로 구분자 선언부 제거
    - InputParser.splitByDelimiter()에 delimiter 파라미터 추가

--- src/main/java/calculator/InputParser.java
+++ src/main/java/calculator/InputParser.java
@@ -2,12 +2,35 @@ package calculator;
 
 public class InputParser {
     private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SEPARATOR = "\\n";
 
     public String[] parse(String input) {
-        return splitByDelimiter(input);
+        if (hasCustomDelimiter(input)) {
+            String customDelimiter = extractCustomDelimiter(input);
+            String content = removeDelimiterPrefix(input);
+            return splitByDelimiter(content, customDelimiter);
+        }
+
+        return splitByDelimiter(input, DEFAULT_DELIMITER);
     }
 
-    private String[] splitByDelimiter(String input) {
-        return input.split(InputParser.DEFAULT_DELIMITER);
+    private String[] splitByDelimiter(String input, String delimiter) {
+        return input.split(delimiter);
+    }
+
+    private boolean hasCustomDelimiter(String input) {
+        return input.startsWith(CUSTOM_DELIMITER_PREFIX);
+    }
+
+    public String extractCustomDelimiter(String input) {
+        int startIndex = CUSTOM_DELIMITER_PREFIX.length();
+        int endIndex = input.indexOf(CUSTOM_DELIMITER_SEPARATOR);
+        return input.substring(startIndex, endIndex);
+    }
+
+    public String removeDelimiterPrefix(String input) {
+        int newlineIndex = input.indexOf(CUSTOM_DELIMITER_SEPARATOR);
+        return input.substring(newlineIndex + 1);
     }
 }
